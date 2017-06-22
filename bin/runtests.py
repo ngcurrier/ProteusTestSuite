b812db9c4b2ae9a6243863f70f520d438cea52b2
Nicholas Currier <nicholas.currier@gmail.com>
2017-06-21 18:33:45 -0700

    update tests script

--- bin/runtests.py
+++ bin/runtests.py
@@ -4,34 +4,58 @@ import os
 import glob
 import datetime
 import fnmatch
+import subprocess
 from xml.dom import minidom
 
 def main():
     run_command = 'mpirun -np '
     
     path = '../'
+    # find all folders with test.xml present
     configfiles = [os.path.join(dirpath, f)
          for dirpath, dirnames, files in os.walk(path)
          for f in fnmatch.filter(files, 'test.xml')]
+
+    print configfiles
     
     for file in configfiles:
+        print 'TEST DEFINITION  --> ' + file
+        dirt = file.rsplit('/',1)[0]
+        # change path to test directory
+        os.chdir(dirt)
+        print '** Directory: ' + os.getcwd()
+        print '=================================='
         xmldoc = minidom.parse(file)
         elementList = xmldoc.getElementsByTagName('exe')
         exe = elementList[0].firstChild.nodeValue
-        print 'exe = ' + exe
+        print '** exe = ' + exe
         elementList = xmldoc.getElementsByTagName('np')
         np = elementList[0].firstChild.nodeValue
-        print 'np = ' + np
+        print '** np = ' + np
         elementList = xmldoc.getElementsByTagName('recomp')
         recomp = elementList[0].firstChild.nodeValue
-        print 'recomp = ' + recomp
+        print '** recomp = ' + recomp
         elementList = xmldoc.getElementsByTagName('decomp')
         decomp = elementList[0].firstChild.nodeValue
-        print 'decomp = ' + decomp
+        print '** decomp = ' + decomp
         elementList = xmldoc.getElementsByTagName('diff')
         diff = elementList[0].firstChild.nodeValue
-        print 'diff = ' + diff
+        print '** diff = ' + diff
 
+        # do the decomp
+        p = subprocess.Popen(decomp, stdout=subprocess.PIPE, shell=True)
+        print str(p.communicate()[0])
+
+        # do the solution
+        p = subprocess.Popen('mpirun -np ' + np + ' ' + exe, stdout=subprocess.PIPE, shell=True)
+        print str(p.communicate()[0])
+
+        
+        # do the recomp
+        p = subprocess.Popen(recomp, stdout=subprocess.PIPE, shell=True)
+        print str(p.communicate()[0])
+        
+        print
 
     return
 
