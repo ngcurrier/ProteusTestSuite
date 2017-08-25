7d1c063a17c56d74d979be1ff4ec22ff7b59b422
Nick Currier <nicholas.currier@gmail.com>
2017-08-25 15:09:34 -0700

    Update runtests.py to ask for number of procs and use universally. Update gitignore. Update test.xml files

--- bin/runtests.py
+++ bin/runtests.py
@@ -8,10 +8,9 @@ import subprocess
 from xml.dom import minidom
 from shutil import copyfile
 import stat
+import sys
 
-def main():
-    run_command = 'mpirun -np '
-    
+def main(numProcs):
     path = '../'
     # find all folders with test.xml present
     configfiles = [os.path.join(dirpath, f)
@@ -34,38 +33,32 @@ def main():
         print '** Directory: ' + os.getcwd()
         print '=================================='
         xmldoc = minidom.parse(file)
-        elementList = xmldoc.getElementsByTagName('exe')
-        exe = elementList[0].firstChild.nodeValue
-        print '** exe = ' + exe
-        elementList = xmldoc.getElementsByTagName('np')
-        np = elementList[0].firstChild.nodeValue
-        print '** np = ' + np
-        elementList = xmldoc.getElementsByTagName('recomp')
-        recomp = elementList[0].firstChild.nodeValue
-        print '** recomp = ' + recomp
-        elementList = xmldoc.getElementsByTagName('decomp')
-        decomp = elementList[0].firstChild.nodeValue
-        print '** decomp = ' + decomp
+        elementList = xmldoc.getElementsByTagName('gridname')
+        gridname = elementList[0].firstChild.nodeValue
+        print '** exe = ' + 'ucs.x'
+        print '** np = ' + str(numProcs)
+        print '** recomp = ' + 'urecomp.x'
+        print '** decomp = ' + 'udecomp.x'
         elementList = xmldoc.getElementsByTagName('diff')
         diff = elementList[0].firstChild.nodeValue
         print '** diff = ' + diff
 
         # do the decomp
-        p = subprocess.Popen(decomp, stdout=subprocess.PIPE, shell=True)
+        p = subprocess.Popen('udecomp.x ' + gridname + ' ' + str(numProcs), stdout=subprocess.PIPE, shell=True)
         print str(p.communicate()[0])
 
         # strip off the casename from recomp
-        case = recomp.split()[-1]
+        case = gridname.split('.')[-2]
         
         # do the solution
         print '----- RUNNING PROTEUS CFD ----- '
-        p = subprocess.Popen('mpiexec -np ' + np + ' ' + exe + ' ' + case, stdout=subprocess.PIPE, shell=True)
+        p = subprocess.Popen('mpiexec -np ' + str(numProcs) + ' ucs.x ' + case, stdout=subprocess.PIPE, shell=True)
         print str(p.communicate()[0])
         print '----- FINISHED RUNNING PROTEUS CFD ----- '
 
         
         # do the recomp
-        p = subprocess.Popen(recomp, stdout=subprocess.PIPE, shell=True)
+        p = subprocess.Popen('urecomp.x ' + case, stdout=subprocess.PIPE, shell=True)
         print str(p.communicate()[0])
         
 
@@ -77,4 +70,7 @@ def main():
     return
 
 if __name__ == "__main__":
-    main()
+    if(len(sys.argv) ==  2):
+        main(int(sys.argv[1]))
+    else:
+        print('USAGE: ' + sys.argv[0] + ' <number of processors>')
