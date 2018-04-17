cabd8fb0df181fbba79df1b15790b50e73e03130
Nicholas Currier <nicholas.currier@gmail.com>
2018-04-16 21:13:37 -0700

    Update sphere test problem to make use of new python interface

---
+++ ConductionSphere/setInitialConditions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+def setInitialConditions(Qinf, neqn, naux, Qset, CoordsXYZ):
+    import numpy as np
+
+    #print Qinf[0]
+    
+    #Qinf is the raw value of farfield variables (non-dimensional) - DO NOT MODIFY
+    #print neqn+ naux
+    qinfSize = neqn + naux
+    #print 'Vector size', qinfSize
+    
+    #Coords array(non-dimensional) [x,y,z]
+    x = CoordsXYZ[0]
+    y = CoordsXYZ[1]
+    z = CoordsXYZ[2]
+    print x,y,z
+
+    #Note that computeAuxiliaryVariables() is called after the set to ensure consistency
+    #You do not have to set those locations to anything meaningfu
+    naux = qinfSize - neqn
+
+    #Qset should be set to desired values (non-dimensional)
+    Qset[0] = 100.0*abs(x)
+    
+    return 1
